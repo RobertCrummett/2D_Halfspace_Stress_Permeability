949219d25faa5e42ee5fe9e45176ab6b969063af
RobertNateCrummett <rncrummett5@gmail.com>
2022-07-24 16:37:31 -0700

    updating functions, adding video results

--- Function_Trial_File.m
+++ Function_Trial_File.m
@@ -6,20 +6,22 @@
 %% User Inputs
 clear; clf; close all;
 
+% Video File
+vfileNamePerm = "H1300A16000_retreat_perm";
+vfileNameLith = "H1300A16000_retreat_lith";
+vfileNamePermLith = "H1300A16000_retreat_permlith";
+
+vfileFrameRate = 15;
+
 % Plot Feild and Phi
-feild2Plot = "XX"; % inputs: "XX","XZ","ZZ","Normal","Shear"
 phi = 60; %degree from horizontal to evaluate normal/shear stress on
 grdRho = 2.3; % g cm^-3, density of the ground
 
 % Line-Load "Glacier"
-initialHeight = 1000; % m
-finalHeight = 700; % m
-initialHalfWidth = 2000; % m
-finalHalfWidth = 2000; % m
-
-% Video File
-vfileName = "TestFile";
-vfileFrameRate = 10;
+initialHeight = 1300; % m
+finalHeight = 1300; % m
+initialHalfWidth = 16000; % m
+finalHalfWidth = 1000; % m
 
 % Display
 iter = 100; % iterations, increase for higher resolution in video file
@@ -36,17 +38,25 @@ No = rho*g*h.*a; %N m^-1, the line load
 A = No./(2*pi*a); % N m^-2
 
 %% Stress Feild Calculation
-step = 25; % m
-x = 0:step:5000; % m
-z = 0:step:3000; % m
+step = 100; % m
+x = 0:step:17000; % m
+step = 25;
+z = 0:step:3500; % m
 
 tic
 [sigma_xx, sigma_xz, sigma_zz] = XZ_Stress(A, x, z, a);
 sigma_normal = Normal_Stress(sigma_xx, sigma_xz, sigma_zz, phi);
-sigma_shear = Shear_Stress(sigma_xx, sigma_xz, sigma_zz, phi);
-sigma_lith = Lithostatic_Stress(2.2, x, z, h, g);
-fail_potential = Failure_Potential(sigma_shear, sigma_normal, sigma_lith);
+sigma_lith = Lithostatic_Stress(grdRho,x,z,h,g);
+k = Permeability_Ratio_Calculator(sigma_normal);
+k_2 = Permeability_Ratio_Calculator(sigma_normal - sigma_lith);
 toc
 
 display_str = [0.7, 0, 0];
-F = Plot_Failure_Potential(x, z, h, a, phi, fail_potential, display_str);
+G = Plot_Permeability_Ratio(x,z,h,a,phi,k);
+H = Plot_Stress_Feild(x, z, h, a, phi, sigma_normal - sigma_lith,"Normal");
+I = Plot_Permeability_Ratio(x,z,h,a,phi,k_2);
+
+%% Video File
+Create_Video_File(G,vfileNamePerm,vfileFrameRate)
+Create_Video_File(H,vfileNameLith,vfileFrameRate)
+Create_Video_File(I,vfileNamePermLith,vfileFrameRate)
